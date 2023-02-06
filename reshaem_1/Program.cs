8f446538b25251d08ac41e5badb5e0c9ad5c612f
arteeex187@gmail.com <arteeex187@gmail.com>
2023-02-06 23:38:23 +0700

    Zadacha_50

---
+++ reshaem_1/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер строки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] arrayTable = new int [10,10];
+FillArrayRandomNumbers(arrayTable);
+
+
+PrintArray(arrayTable);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-50, 50);
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
+if (n > arrayTable.GetLength(0) || m > arrayTable.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {n} строки и {m} столбца равно:  {arrayTable[n-1,m-1]}");
+}
